eb2fc9c956dbcdffb14a8a16457675b9d9828665
LegalCheng <100748628+LegalCheng@users.noreply.github.com>
2022-04-29 20:07:22 +0800

    Update dbb.php

--- www/dbb.php
+++ www/dbb.php
@@ -1,10 +1,6 @@
 <?php 
     session_start();
-    define('DB_SERVER', 'db');
-    define('DB_USERNAME', 'user1000');
-    define('DB_PASSWORD', 'kiki90317');
-    define('DB_NAME', 'myDb');
-    $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+    require_once('config.php');
     if($_SESSION['token']!=$_POST['token']){
         echo "<script>alert('CSRF')</script>";
     }else{
