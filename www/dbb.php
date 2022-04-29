5425feca647d86f9efa121b68b23eb5c9f090177
LegalCheng <100748628+LegalCheng@users.noreply.github.com>
2022-04-29 21:36:42 +0800

    Update dbb.php

--- www/dbb.php
+++ www/dbb.php
@@ -4,17 +4,20 @@
     if($_SESSION['token']!=$_POST['token']){
         echo "<script>alert('CSRF')</script>";
     }else{
-        $id=$_GET['id'];
+        $id=intval($_GET['id']);
         $mail=$_SESSION['E-mail'];
-        $sql1 = "SELECT * FROM `comment` WHERE `id`='$id'";
-        $result1=mysqli_query($link,$sql1);
-        $nums=mysqli_fetch_row($result1);
-        if($nums[4]!=$mail){
+        $stmt = $link->prepare("SELECT * FROM `comment` WHERE `id` = ?;");
+        $stmt->bind_param('i',$id);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $output = $result->fetch_assoc();
+        if($output['E-mail']!=$mail){
             echo "<script>alert('你無法刪除別人的留言')</script>";
         }
         else{
-            $sql = "DELETE FROM comment WHERE id = '$id'";
-            $result=mysqli_query($link,$sql);   
+            $stmt = $link->prepare("DELETE FROM `comment` WHERE `id`= ?;");
+            $stmt->bind_param('i',$id);
+            $stmt->execute();   
         }
     }
     echo "<script>location.href='comment.php'</script>";
