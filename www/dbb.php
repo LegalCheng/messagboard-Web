a2fb370e83b5fa66dd4c8ba604340f202d1ec394
LegalCheng <100748628+LegalCheng@users.noreply.github.com>
2022-04-29 19:47:05 +0800

    Update dbb.php

--- www/dbb.php
+++ www/dbb.php
@@ -5,17 +5,21 @@
     define('DB_PASSWORD', 'kiki90317');
     define('DB_NAME', 'myDb');
     $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
-    $id=$_GET['id'];
-    $mail=$_SESSION['E-mail'];
-    $sql1 = "SELECT * FROM `comment` WHERE `id`='$id'";
-    $result1=mysqli_query($link,$sql1);
-    $nums=mysqli_fetch_row($result1);
-    if($nums[4]!=$mail){
-        echo "<script>alert('你無法刪除別人的留言')</script>";
-    }
-    else{
-        $sql = "DELETE FROM comment WHERE id = '$id'";
-        $result=mysqli_query($link,$sql);   
+    if($_SESSION['token']!=$_POST['token']){
+        echo "<script>alert('CSRF')</script>";
+    }else{
+        $id=$_GET['id'];
+        $mail=$_SESSION['E-mail'];
+        $sql1 = "SELECT * FROM `comment` WHERE `id`='$id'";
+        $result1=mysqli_query($link,$sql1);
+        $nums=mysqli_fetch_row($result1);
+        if($nums[4]!=$mail){
+            echo "<script>alert('你無法刪除別人的留言')</script>";
+        }
+        else{
+            $sql = "DELETE FROM comment WHERE id = '$id'";
+            $result=mysqli_query($link,$sql);   
+        }
     }
     echo "<script>location.href='comment.php'</script>";
 ?>
