415fe003f6ef50c12859b3136cec8963abfae173
LegalCheng <100748628+LegalCheng@users.noreply.github.com>
2022-04-29 19:47:27 +0800

    Update detail.php

--- www/detail.php
+++ www/detail.php
@@ -7,9 +7,11 @@
     $id=$_GET['id'];
     $data = mysqli_query($link, "SELECT * FROM comment WHERE `id` = '$id' ");
     $nums=mysqli_fetch_row($data);
+    $name=htmlspecialchars($nums[1],ENT_NOQUOTES);
+    $filename=htmlspecialchars($nums[8]);
     echo "<h2>POST</h2>";
-    echo "<h3>Poster :$nums[1]</h3>";
+    echo "<h3>Poster :$name</h3>";
     echo "<h3>Content :$nums[2]</h3>";
     echo "<h3>Time :$nums[3]</h3>";
-    echo "<h3>Attach :$nums[8]</h3>";
+    echo "<h3>Attach :$filename</h3>";
 ?>
