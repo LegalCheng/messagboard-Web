920351971998a3d2066f9f0e3ef9172b791f7deb
LegalCheng <100748628+LegalCheng@users.noreply.github.com>
2022-04-29 21:37:09 +0800

    Update detail.php

--- www/detail.php
+++ www/detail.php
@@ -1,13 +1,19 @@
 <?php 
     require_once('config.php');
     $id=$_GET['id'];
-    $data = mysqli_query($link, "SELECT * FROM comment WHERE `id` = '$id' ");
-    $nums=mysqli_fetch_row($data);
-    $name=htmlspecialchars($nums[1],ENT_NOQUOTES);
-    $filename=htmlspecialchars($nums[8]);
+    $stmt = $link->prepare("SELECT * FROM `comment` WHERE `id` = ?;");
+    $stmt->bind_param('i',$id);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $output = $result->fetch_assoc();
+    
+    $name=htmlspecialchars($output['nickname'],ENT_NOQUOTES);
+    $filename=htmlspecialchars($output['dataname']);
+    $content=$output['comment'];
+    $time=$output['time'];
     echo "<h2>POST</h2>";
     echo "<h3>Poster :$name</h3>";
-    echo "<h3>Content :$nums[2]</h3>";
-    echo "<h3>Time :$nums[3]</h3>";
+    echo "<h3>Content :$content</h3>";
+    echo "<h3>Time :$time</h3>";
     echo "<h3>Attach :$filename</h3>";
 ?>
