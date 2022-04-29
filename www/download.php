af77066fce19e37aeb67e2b5dee8b272aa26629d
LegalCheng <100748628+LegalCheng@users.noreply.github.com>
2022-04-29 20:07:55 +0800

    Update download.php

--- www/download.php
+++ www/download.php
@@ -1,9 +1,5 @@
 <?php
-    define('DB_SERVER', 'db');
-    define('DB_USERNAME', 'user1000');
-    define('DB_PASSWORD', 'kiki90317');
-    define('DB_NAME', 'myDb');
-    $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+    require_once('config.php');
     $id=$_GET['FileNo'];
     $query="SELECT * FROM comment WHERE `id` = '$id' ";
     $ret=mysqli_query($link,$query);
