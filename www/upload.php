899385779e6a9a1cdeb7d7f0d09536cccf9ca930
LegalCheng <100748628+LegalCheng@users.noreply.github.com>
2022-04-29 20:09:04 +0800

    Update upload.php

--- www/upload.php
+++ www/upload.php
@@ -1,21 +1,29 @@
 <?php 
 session_start();
-define('DB_SERVER', 'db');
-define('DB_USERNAME', 'user1000');
-define('DB_PASSWORD', 'kiki90317');
-define('DB_NAME', 'myDb');
-$conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+require_once('config.php');
 $username = $_SESSION['E-mail'];
 if($_POST['url']!=null){
     $url=$_POST['url'];//圖片所在網址
-    $type=getimagesize($url);//取得圖片資訊
-    $fileContent = base64_encode(file_get_contents($url));
+    if(substr($url,0,8)!="https://"){
+        echo "<script>alert('不是https網址不給你改大頭貼')</script>";
+        echo "<script>location.href='comment.php'</script>";
+    }
+    else{
+        if(preg_match('/.*(\.png|\.jpg|\.jpeg|\.gif)$/', $url)){
+            $type=getimagesize($url);//取得圖片資訊
+            $fileContent = base64_encode(file_get_contents($url));
+        }
+        else{
+            echo "<script>alert('不是圖片不給你改大頭貼')</script>";
+            echo "<script>location.href='comment.php'</script>";
+        }
+    }
 }else{
     $error = $_FILES['file']['error'];
     $type = $_FILES['file']['type']; // 圖片類型
-    if ($error === 0) {
-        $tmpname = $_FILES['file']['tmp_name']; // 圖片暫存位置
-        switch($type) {
+    $tmpname = $_FILES['file']['tmp_name']; // 圖片暫存位置
+    $safeType=false;
+    switch($type) {
         case 'image/jpeg':
             $safeType = true;
             break;
@@ -25,7 +33,6 @@ if($_POST['url']!=null){
         case 'image/png':
             $safeType = true;
             break;
-        }
     }
     if ($safeType) {
         $file = fopen($tmpname, 'rb'); // 以二進位制開啟圖片
@@ -33,11 +40,16 @@ if($_POST['url']!=null){
         fclose($file); // 關閉圖片
         $fileContent = base64_encode($fileContent);// 將圖片編碼成 Base64 文字
     }
+    else{
+        echo "<script>alert('不是圖片不給你改大頭貼')</script>";
+        echo "<script>location.href='comment.php'</script>";
+    }
     $sql = "UPDATE `members` SET `picture` = '$fileContent', `type`='$type' WHERE `E-mail` = '$username'";
-    mysqli_query($conn,$sql);
+    mysqli_query($link,$sql);
     header("Location: comment.php");
 }
 $sql = "UPDATE `members` SET `picture` = '$fileContent', `type`='$type' WHERE `E-mail` = '$username'";
-mysqli_query($conn,$sql);
+mysqli_query($link,$sql);
 echo "<script>location.href='comment.php'</script>"
 ?>
+
